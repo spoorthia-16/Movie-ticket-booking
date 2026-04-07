bde42824694364a2e89f7b12307dadefb509d97a
Student Apna <spoorthilogin016@gmail.com>
2026-04-07 19:49:38 +0530

    send confirmation email

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,9 @@
+import { model } from "mongoose";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 import User from "../models/User.js";
 import { Inngest, step } from "inngest";
+import sendEmail from "../configs/nodeMailer.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
@@ -106,9 +108,39 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
   }
 )
 
+//Inngest func to send emails when user books a show
+const sendBookingConfirmarionEmail = inngest.createFunction(
+  {id: "send-booking-confirmation-email"},
+  {event: "app/show.booked"},
+  async ({ event, step})=>{
+    const {bookingId} = event.data;
+
+    const booking = await Booking.findById(bookingId).populate({
+      path:'show',
+      populate: {path: "movie",model:"Movie"}
+    }).populate('user');
+
+    await sendEmail({
+      to: booking.user.email,
+      subject : `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+      body : `<div style="font-family: Arial,sans-serif; line-height: 1.5;">
+      <h2>Hi ${booking.user.name},</h2>
+      <p>Your booking for<strong style="color:#F84565;"> "${booking.show.movie.title}" </strong> is confirmed.</p>
+      <p>
+        <strong>Date:</strong> ${new Date(booking.show.showDate).toLocaleDateString('en-US', {timeZone: 'Asia/Kolkata'})} <br/>
+        <strong>Time:</strong> ${new Date(booking.show.showDate).toLocaleDateString('en-US', {timeZone: 'Asia/Kolkata'})} 
+        </p>
+        <p>Enjoy the show! </p>
+        <p> Thanks for booking with us! <br/> -QuickShow Team </p>
+      </div>`
+    })
+  }
+)
+
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
-  releaseSeatsAndDeleteBooking
+  releaseSeatsAndDeleteBooking,
+  sendBookingConfirmarionEmail
 ];
